87b92264955c245ec91db04221030351ac89b0ae
Christian Radden <102736612+chradden@users.noreply.github.com>
2023-06-28 21:59:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,96 +1,83 @@
 import streamlit as st
-from PIL import Image
-import os
-# import langchain
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-# from langchain import OpenAI, VectorDBQA
-# from langchain.chains import RetrievalQAWithSourcesChain
-# import PyPDF2
-from advertools import crawl
-import pandas as pd
-from langchain.document_loaders import DataFrameLoader
-from langchain.text_splitter import CharacterTextSplitter,RecursiveCharacterTextSplitter
-from langchain import OpenAI, VectorDBQA
+import langchain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain import OpenAI, VectorDBQA
+from langchain.chains import RetrievalQAWithSourcesChain
+import PyPDF2
 
-
-st.set_page_config(layout="centered", page_title="Website QnA")
-# image = Image.open('GeekAvenue_logo.png')
- 
-
-# col1, mid, col2 = st.columns([1,2,20])
-# with col1:
-#     st.image(image, width=80)
-# with col2:
-#     st.header('Geek Avenue')
-# st.write("---") # horizontal separator line.
-
-
-st.header("Website QnA Bot 🤖")
-state = st.session_state
-site = st.text_input("Enter your URL here")
-if st.button("Build Model"):
+#This function will go through pdf and extract and return list of page texts.
+def read_and_textify(files):
+    text_list = []
+    sources_list = []
+    for file in files:
+        pdfReader = PyPDF2.PdfReader(file)
+        #print("Page Number:", len(pdfReader.pages))
+        for i in range(len(pdfReader.pages)):
+          pageObj = pdfReader.pages[i]
+          text = pageObj.extract_text()
+          pageObj.clear()
+          text_list.append(text)
+          sources_list.append(file.name + "_page_"+str(i))
+    return [text_list,sources_list]
   
- 
-  if site is None:
-    st.info(f"""Enter Website to Build QnA Bot""")
-  elif site:
-   
-    st.write(str(site) + " starting to crawl..")
+st.set_page_config(layout="centered", page_title="Multidoc_QnA")
+st.header("Multidoc_QnA")
+st.write("---")
+  
+#file uploader
+uploaded_files = st.file_uploader("Upload documents",accept_multiple_files=True, type=["txt","pdf"])
+st.write("---")
+
+if uploaded_files is None:
+  st.info(f"""Upload files to analyse""")
+elif uploaded_files:
+  st.write(str(len(uploaded_files)) + " document(s) loaded..")
+  
+  textify_output = read_and_textify(uploaded_files)
+  
+  documents = textify_output[0]
+  sources = textify_output[1]
+  
+  #extract embeddings
+  embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
+  #vstore with metadata. Here we will store page numbers.
+  vStore = Chroma.from_texts(documents, embeddings, metadatas=[{"source": s} for s in sources])
+  #deciding model
+  model_name = "gpt-3.5-turbo"
+  # model_name = "gpt-4"
+
+  retriever = vStore.as_retriever()
+  retriever.search_kwargs = {'k':2}
+
+  #initiate model
+  llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"], streaming=True)
+  model = RetrievalQAWithSourcesChain.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+  
+  st.header("Ask your data")
+  user_q = st.text_area("Enter your questions here")
+  
+  if st.button("Get Response"):
     try:
-
-      my_bar = st.progress(0, text="Crawling in progress. Please wait.")
-      if os.path.exists("simp.jl"):
-        os.remove("simp.jl")
-      crawl(site, 'simp.jl', follow_links=False)
-      crawl_df = pd.read_json('simp.jl', lines=True)
-      st.write(len(crawl_df))
-      crawl_df = crawl_df[['body_text']]
-      my_bar.progress(50, text="Building Vector DB.")
-      st.write(crawl_df)
-
-      #load df to langchain
-      loader = DataFrameLoader(crawl_df, page_content_column="body_text")
-      docs = loader.load()
-
-      #chunking
-      text_splitter = RecursiveCharacterTextSplitter(
-      chunk_size=1000, chunk_overlap=0, separators=[" ", ",", "\n"]
-      )
-      doc_texts = text_splitter.split_documents(docs)
-
-
-      #extract embeddings and build QnA Model
-      openAI_embeddings = OpenAIEmbeddings(openai_api_key = st.secrets["openai_api_key"])
-      vStore = Chroma.from_documents(doc_texts, openAI_embeddings)
-
-      # Initialize VectorDBQA Chain from LangChain
-      #deciding model
-      model_name = "gpt-3.5-turbo"
-      llm = OpenAI(model_name=model_name, openai_api_key = st.secrets["openai_api_key"])
-      model = VectorDBQA.from_chain_type(llm=llm, chain_type="stuff", vectorstore=vStore)
-      my_bar.progress(100, text="Model is ready.")
-      st.session_state['crawling'] = True
-      st.session_state['model'] = model
-      st.session_state['site'] = site
-
+      with st.spinner("Model is working on it..."):
+        result = model({"question":user_q}, return_only_outputs=True)
+        st.subheader('Your response:')
+        st.write(result['answer'])
+        st.subheader('Source pages:')
+        st.write(result['sources'])
     except Exception as e:
-              st.error(f"An error occurred: {e}")
-              st.error('Oops, crawling resulted in an error :( Please try again with a different URL.')
-     
-if site and ("crawling" in state):
-      st.header("Ask your data")
-      model = st.session_state['model']
-      user_q = st.text_input("Enter your questions here")
-      if st.button("Get Response"):
-        try:
-          with st.spinner("Model is working on it..."):
-#             st.write(model)
-            result = model({"query":user_q}, return_only_outputs=True)
-            st.subheader('Your response:')
-            st.write(result["result"])
-        except Exception as e:
-          st.error(f"An error occurred: {e}")
-          st.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')
+      st.error(f"An error occurred: {e}")
+      st.error('Oops, the GPT response resulted in an error :( Please try again with a different question.')
+      
+        
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
